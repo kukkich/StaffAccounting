ee8a35f400ee66050fcbec453685ba16d673cae2
vitia <vitia.iezhovqqqq@gmail.com>
2022-05-29 13:03:35 +0700

    add Index and details views for Ranks, Project and Department

--- Models/Company/Project.cs
+++ Models/Company/Project.cs
@@ -1,10 +1,18 @@
 ﻿namespace StaffAccounting.Models.Company
 {
-    public class Project
+    public class Project : IDataJoinable
     {
         public int Id { get; set; }
         public string? Name { get; set; }
 
         public List<Manager> Managers { get; set; } = new();
+
+        public void JoinFromDatabase(CompanyContext context)
+        {
+            Managers = context.Managers.Where(manager => manager.ProjectId == Id).ToList();
+        }
+
+        public Task JoinFromDatabaseAsync(CompanyContext context)
+            => Task.Run(() => JoinFromDatabase(context));
     }
 }
--- Models/Company/Rank.cs
+++ Models/Company/Rank.cs
@@ -1,10 +1,18 @@
 ﻿namespace StaffAccounting.Models.Company
 {
-    public class Rank
+    public class Rank : IDataJoinable
     {
         public int Id { get; set; }
         public string? Name { get; set; }
 
         public List<Worker> Workers { get; set; } = new();
+
+        public void JoinFromDatabase(CompanyContext context)
+        {
+            Workers = context.Workers.Where(worker => worker.RankId == Id).ToList();
+        }
+
+        public Task JoinFromDatabaseAsync(CompanyContext context)
+            => Task.Run(() => JoinFromDatabase(context));
     }
 }
