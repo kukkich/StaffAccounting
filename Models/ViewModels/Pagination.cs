51660e787f0942f74a1055179701e32b585e550d
vitia <vitia.iezhovqqqq@gmail.com>
2022-06-01 10:24:16 +0700

    add pagination models

---
+++ Models/ViewModels/Pagination.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StaffAccounting.Models.ViewModels
+{
+    public class Pagination<TItems>
+    {
+        public int PageSize { get; private set; }
+        public int PageNumber
+        {
+            get => _paging.PageNumber;
+            set => _paging.PageNumber = value;
+        }
+        public int TotalPages => _paging.TotalPages;
+        public bool HasNextPage => _paging.HasNextPage;
+        public bool HasPreviousPage => _paging.HasPreviousPage;
+
+        private readonly Paging _paging;
+        private readonly IQueryable<TItems> _source;
+
+        public Pagination(IQueryable<TItems> collection, int pageSize)
+        {
+            _source = collection;
+            PageSize = pageSize;
+            _paging = new Paging(collection.Count(), PageSize, 1);
+        }
+
+        public List<TItems> GetItems()
+        {
+            return _source.Skip((_paging.PageNumber - 1) * PageSize)
+                .Take(PageSize)
+                .ToList();
+        }
+
+        public async Task<List<TItems>> GetItemsAsync()
+        {
+            return await _source.Skip((_paging.PageNumber - 1) * PageSize)
+                .Take(PageSize)
+                .ToListAsync();
+        }
+    }
+}
---
+++ Models/ViewModels/Paging.cs
@@ -0,0 +1,34 @@
+﻿namespace StaffAccounting.Models.ViewModels
+{
+    public class Paging
+    {
+        public int PageNumber 
+        {
+            get => _pageNumber; 
+            set {
+                if (!IsCorrectPageNumber(value))
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                _pageNumber = value;
+            }
+        }
+        public int TotalPages { get; private set; }
+        public bool HasNextPage => PageNumber < TotalPages;
+        public bool HasPreviousPage => PageNumber > 1;
+
+        private int _pageNumber;
+
+        public Paging(int totalCount, int pageSize, int pageNumber)
+        {
+            if (totalCount < 0) throw new ArgumentOutOfRangeException(nameof(totalCount));
+            if (pageSize <= 0) throw new ArgumentOutOfRangeException(nameof(pageSize));
+
+            TotalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+            PageNumber = pageNumber;
+        }
+
+        private bool IsCorrectPageNumber(int pageNumber)
+        {
+            return pageNumber > 0 && pageNumber <= TotalPages;
+        }
+    }
+}
